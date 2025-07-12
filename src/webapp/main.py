26cb1d4cab90c0777c03155c2ea55ef27a22f492
vasgg <vas.g@icloud.com>
2025-07-12 22:15:40 +0400

    working on webapp

--- src/webapp/main.py
+++ src/webapp/main.py
@@ -42,5 +42,10 @@ setup_logs("yookassa_webhook")
 
 app.include_router(webhook_router, prefix="/webhook")
 
+
+def run_main():
+    uvicorn.run("src.webapp.main:app", host="0.0.0.0", port=8080, reload=True)
+
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+    run_main()

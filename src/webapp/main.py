91b5437a206e5b61df3f8b8b9f5934f2503b91ce
vasgg <vas.g@icloud.com>
2025-07-12 22:25:48 +0400

    working on webapp

--- src/webapp/main.py
+++ src/webapp/main.py
@@ -44,7 +44,7 @@ app.include_router(router, prefix="/webhook")
 
 
 def run_main():
-    uvicorn.run("src.webapp.main:app", host="0.0.0.0", port=8080)
+    uvicorn.run("webapp.main:app", host="0.0.0.0", port=8080)
 
 
 if __name__ == "__main__":
